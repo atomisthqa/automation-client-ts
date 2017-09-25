03365ea3da6cc717b04860d237dc786edff247b5
Christian Dupuis <cd@atomist.com>
2017-09-25 22:18:39 +0200

    Automatic de-linting [atomist:auto-delint]

--- src/automationClient.ts
+++ src/automationClient.ts
@@ -12,7 +12,7 @@ import { WebSocketClient, WebSocketClientOptions } from "./internal/transport/we
 import { WebSocketTransportEventHandler } from "./internal/transport/websocket/WebSocketTransportEventHandler";
 import { logger } from "./internal/util/logger";
 import { AutomationServer } from "./server/AutomationServer";
-import { BuildableAutomationServer } from "./server/BuildableAutomationServer"
+import { BuildableAutomationServer } from "./server/BuildableAutomationServer";
 
 export const DefaultStagingAtomistServer =
     "https://automation-staging.atomist.services/registration";
@@ -35,7 +35,7 @@ export class AutomationClient {
                 teamId: configuration.teamId
                 keywords: [],
                 token: process.env.GITHUB_TOKEN
-                graphqlEndpoint: DefaultStagingAtomistGraphQLServer
+                graphqlEndpoint: DefaultStagingAtomistGraphQLServer,
             });
     }
 
